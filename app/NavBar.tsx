6bdc430746d04233c96fc6bb951ee5e50e43e291
Karan Kuthe <karankuthe132@gmail.com>
2025-01-23 02:22:35 +0530

    styling the active link

--- app/NavBar.tsx
+++ app/NavBar.tsx
@@ -1,12 +1,29 @@
+'use client'
 import React from 'react'
 import { FaBug } from "react-icons/fa";
 import Link from 'next/link'
+import { usePathname } from 'next/navigation';
+import classNames from 'classnames'
 export default function NavBar() {
+ const currentPath = usePathname()
   return (
     <nav className='border border-gray-300 w-full h-[60px] flex gap-6 justify-start items-center p-4'>
        <Link href='/'><FaBug className="text-black" /></Link> 
-        <h1 className='text-zinc-500 hover:text-zinc-800'><Link href='/'>Dashboard</Link></h1>
-        <h1 className='text-zinc-500 hover:text-zinc-800'><Link href='/issues'>Issues</Link></h1>
+        <h1>
+            <Link href='/' className={classNames({
+                 'text-zinc-900': currentPath === '/',
+                 'text-zinc-500': currentPath !== '/',
+            })}
+            >Dashboard</Link>
+        </h1>
+        <h1>
+        <Link href="/issues" className={classNames({
+          'text-zinc-900': currentPath === '/issues',
+          'text-zinc-500': currentPath !== '/issues',
+        })}>
+          Issues
+        </Link>
+      </h1>
     </nav>
   )
 }
