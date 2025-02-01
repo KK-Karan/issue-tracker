e49af9c1aeae9cfb9faf01d9db0db50b8dd5f1d3
Karan Kuthe <karankuthe132@gmail.com>
2025-02-01 16:54:13 +0530

    fetched data with react query

--- app/issues/[id]/AssignSelect.tsx
+++ app/issues/[id]/AssignSelect.tsx
@@ -1,28 +1,42 @@
 "use client";
 
 import { User } from "@prisma/client";
-import { Select } from "@radix-ui/themes";
+import { Select, Skeleton } from "@radix-ui/themes";
+import { useQuery } from "@tanstack/react-query";
 import axios from "axios";
 import React, { useEffect, useState } from "react";
 
 export default function AssignSelect() {
-  const [users, setUsers] = useState<User[]>([]);
+  const {
+    data: users,
+    error,
+    isLoading,
+  } = useQuery<User[]>({
+    queryKey: ["users"],
+    queryFn: async ({ signal }) => {
+      const res = await axios.get("/api/users", {
+        // When you use Axios, the get() method returns an AxiosResponse object
+        signal,
+      });
+      return res.data; // If we don't return response.data, React Query will receive the entire AxiosResponse object instead of the expected User[] array.
+    },
+    staleTime: 60000,
+    retry: 3,
+  });
 
-  useEffect(() => {
-    const fetchUsers = async () => {
-      const {data} = await axios.get<User[]>("/api/users");
-      setUsers(data);
-    };
-    fetchUsers();
-  }, []);
+  if (isLoading) {
+    return <Skeleton />;
+  }
+
+  if (error) return null;
 
   return (
     <Select.Root defaultValue="apple">
       <Select.Trigger />
       <Select.Content>
         <Select.Group>
-          <Select.Label>Suggestions</Select.Label>
-          {users.map((user) => (
+          <Select.Label>AssignSelect</Select.Label>
+          {users?.map((user) => (
             <Select.Item value={user.id}>{user.name}</Select.Item>
           ))}
         </Select.Group>
